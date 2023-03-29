739883720c3790a5fc87c73b11d73e2195e52b85
ddimma <dumitru.pavlov@mib.utm.md>
2023-03-29 20:03:23 +0300

    Minor changes

--- AutoCar.Domain/Entities/User/UDbModel.cs
+++ AutoCar.Domain/Entities/User/UDbModel.cs
@@ -14,23 +14,38 @@ namespace AutoCar.Domain.Entities.User
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         [StringLength(50)]
         public string FullName { get; set; }
+
         [Required]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Email cannot ...")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail id is not valid")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter password!")]
+        [DataType(DataType.Password)]
         [StringLength(30, MinimumLength = 8)]
+        [RegularExpression(@"^([a-zA-Z0-9]{8,15})$", ErrorMessage = "Password must contain: " +
+            "Minimum 8 characters atleast 1 UpperCase Alphabet, " +
+            "1 LowerCase      Alphabet and 1 Number")]
         public string Password { get; set; }
+
         [Required]
         [StringLength(30, MinimumLength = 5)]
         public string UserName { get; set; }
         public string RegisterIP { get; set; }
+
         [Required]
         public bool Terms { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime LoginDateTime { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime RegisterDateTime { get; set; }
+        
         [Required]
         public URole AccessLevel { get; set; }
 
