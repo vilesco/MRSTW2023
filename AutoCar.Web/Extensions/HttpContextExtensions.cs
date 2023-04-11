0a55ccbf962d480d1c41d2daa76ead1ed72b70a5
ddimma <dumitru.pavlov@mib.utm.md>
2023-04-11 20:32:09 +0300

    Minor changes

--- AutoCar.Web/Extensions/HttpContextExtensions.cs
+++ AutoCar.Web/Extensions/HttpContextExtensions.cs
@@ -5,7 +5,16 @@ using System.Web;
 
 namespace WebApplication1.Extensions
 {
-    public class HttpContextExtensions
+    public static class HttpContextExtensions
     {
+        public static UserMinimal GetMySessionObject(this HttpContext current)
+        {
+            return (UserMinimal)current?.Session["__SessionObject"];
+        }
+
+        public static void SetMySessionObject(this HttpContext current, UserMinimal profile)
+        {
+            current.Session.Add("__SessionObject", profile);
+        }
     }
 }
