49464c747de54b892f94e784930a2279e27f10b4
BucsanDan <>
2023-04-25 20:24:10 +0300

    Adding Product and a little bit of the bussiness logic for it.
    Added Service, PostService, ProductContext, Cars/PDbModel
    
    Adding Product and a little bit of the bussiness logic for it.
    
    Added Service, PostService, ProductContext, Cars/PDbModel

--- AutoCar.Web/Controllers/AddPostController.cs
+++ AutoCar.Web/Controllers/AddPostController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using AutoCar.BusinessLogic.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using AutoCar.Domain.Entities.Cars;
 
 namespace AutoCar.Web.Controllers
 {
@@ -13,5 +16,103 @@ namespace AutoCar.Web.Controllers
         {
             return View();
         }
+        PostService Posts = new PostService();
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            // Find the post entry in the database, also load the author entry,
+            // and all the comments.
+            var postResponse = Posts.GetById(id.Value);
+            if (!postResponse.Success)
+                return HttpNoPermission();
+
+            var post = postResponse.Entry;
+            if (post == null)
+                return HttpNotFound();
+
+            Posts.IncrementViewCount(post);
+
+            // Show the full story of the post.
+            return View(post);
+        }
+
+        [RequireUserRole(UserRole.Admin)]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            var post = Posts.GetById(id.Value);
+            if (!post.Success)
+                return HttpNoPermission();
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View(post.Entry);
+        }
+
+        [HttpPost]
+        [RequireUserRole(UserRole.Admin)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Story")] PDbModel post)
+        {
+            if (ModelState.IsValid)
+            {
+                var r = Posts.Edit(post);
+                if (r.Success)
+                    return RedirectToAction("Details");
+
+                return HttpNoPermission();
+            }
+
+            return View(post);
+        }
+
+        [RequireUserRole(UserRole.Admin)]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            var post = Posts.GetById(id.Value);
+            if (!post.Success)
+                return HttpNoPermission();
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View(post.Entry);
+        }
+
+        [HttpPost]
+        [RequireUserRole(UserRole.Admin)]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            var resp = Posts.GetById(id.Value);
+            if (!resp.Success)
+                return HttpNoPermission();
+
+            var post = resp.Entry;
+            if (post == null)
+                return HttpNotFound();
+
+            var delResp = Posts.Delete(post);
+            if (!delResp.Success)
+                return HttpNoPermission();
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
     }
 }
