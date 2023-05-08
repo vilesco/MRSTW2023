4c3d4c82a833aa0d2add7accd357ac64da03a8c4
ddimma <dumitru.pavlov@mib.utm.md>
2023-05-08 17:43:53 +0300

    Setting up post db context and start working on its business logic

---
+++ AutoCar.BusinessLogic/PostBL.cs
@@ -0,0 +1,20 @@
+﻿using AutoCar.BusinessLogic.Core;
+using AutoCar.BusinessLogic.Interfaces;
+using AutoCar.Domain.Entities.Post;
+using AutoCar.Domain.Entities.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCar.BusinessLogic
+{
+    public class PostBL : PostApi, IPost
+    {
+        public ServiceResponse AddPostAction(PDbModel model)
+        {
+            return AddPost(model);
+        }
+    }
+}
--- AutoCar.Web/Extensions/HttpContextExtensions.cs
+++ AutoCar.Web/Extensions/HttpContextExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using AutoCar.Domain.Entities.User;
 
-namespace WebApplication1.Extensions
+namespace AutoCar.Web.Extensions
 {
     public static class HttpContextExtensions
     {
